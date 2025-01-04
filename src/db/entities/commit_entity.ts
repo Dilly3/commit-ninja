501ce0a2b35e5099f12d2ca78c4f81903e78bb71
michael anikammadu <dil.anikamadu@gmail.com>
2025-01-04 15:03:44 +0100

    commit entity

---
+++ src/db/entities/commit_entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class CommitInfo {
+  @PrimaryColumn()
+  id: string;
+  @Column({
+    type: 'varchar',
+    name: 'repo_name',
+    nullable: false,
+  })
+  repoName: string;
+  @Column({ name: 'message', type: 'varchar' })
+  message: string;
+  @Column({ name: 'author_name' })
+  authorName: string;
+  @Column({ name: 'author_email' })
+  authorEmail: string;
+  @Column({ name: 'date' })
+  date: string;
+  @Column({ name: 'url' })
+  url: string;
+}
