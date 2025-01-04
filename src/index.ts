6253cef9acef244fa0f386d50256bab00da9fe10
michael anikammadu <dil.anikamadu@gmail.com>
2025-01-04 16:05:44 +0100

    update index.ts with / route

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,45 @@
-
 import express, { Express, Request, Response } from "express";
 import { AppDataSource } from "./db/database";
-import {config} from "./config/config"
+import { config } from "./config/config";
 
 import bodyParser from "body-parser";
-import Cors from 'cors'
-
+import Cors from "cors";
+import { GithubCommit } from "./github/commit_service";
 
 const app: Express = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use(Cors())
+app.use(Cors());
 
-app.get('/', (req:Request, res : Response) => {
-  console.log(req.body())
-  res.status(200).json({message:'hello World'})
+app.get("/", async (req: Request, res: Response) => {
+  const commitIns = new GithubCommit(
+    config.githubBaseUrl,
+    config.githubOwner,
+    config.githubRepo,
+    config.githubToken,
+    config.startDate,
+  );
+  try {
+    const commits = await commitIns.getCommits();
+    const comms = commits.map((commit) => commitIns.getCommitInstance(commit));
 
-})
+    console.log(req.body);
+    res.send(comms);
+  } catch (error) {
+    console.error("Error fetching commits:", error);
+    res.status(500).send("Error fetching commits");
+  }
+});
 
-AppDataSource.initialize().then(()=> {
-  console.log('connected to db')
-  app.listen(config.port, ()=> {
-  console.log(`listening on port: ${config.port}`)
-})
-}).catch((err : Error) => {
-  console.log('error connecting to db', err.message,err)
-})
-
-
-
-
-   
+// initialize database and app
+AppDataSource.initialize()
+  .then(() => {
+    console.log("connected to db");
+    app.listen(config.port, () => {
+      console.log(`listening on port: ${config.port}`);
+    });
+  })
+  .catch((err: Error) => {
+    console.log("error connecting to db", err.message, err);
+  });
