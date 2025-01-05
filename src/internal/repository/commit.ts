802b46e43ff67a6c8c1c5eb5cdaf87fda9d6a1a8
michael anikammadu <dil.anikamadu@gmail.com>
2025-01-05 10:32:58 +0100

    get date of last commit

--- src/internal/repository/commit.ts
+++ src/internal/repository/commit.ts
@@ -83,4 +83,38 @@ export class CommitRepository {
 
     return await query.getMany();
   }
+
+  async getCommitCountsByAuthor(
+    startDate?: Date,
+    endDate?: Date,
+  ): Promise<Array<CommitSumm>> {
+    const query = this.commitReposit
+      .createQueryBuilder("commit")
+      .select("commit.author_email", "author_email")
+      .addSelect("commit.author_name", "author_name")
+      .addSelect("COUNT(*)", "commit_count");
+
+    if (startDate) {
+      query.andWhere("commit.timestamp >= :startDate", { startDate });
+    }
+    if (endDate) {
+      query.andWhere("commit.timestamp <= :endDate", { endDate });
+    }
+
+    return await query
+      .groupBy("commit.author_email")
+      .orderBy("commit_count", "DESC")
+      .getRawMany();
+  }
+
+  async getDateOfLastCommit(): Promise<string | null> {
+    const mostRecentCommit = await this.commitReposit
+      .createQueryBuilder("commit")
+      .select("commit.date")
+      .orderBy("commit.date", "DESC")
+      .limit(1)
+      .getOne();
+
+    return mostRecentCommit ? mostRecentCommit.date : null;
+  }
 }
