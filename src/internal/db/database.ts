2bdb1394bbee04c08b7046cb003c793bc3baf0a0
michael anikammadu <dil.anikamadu@gmail.com>
2025-01-05 10:28:42 +0100

    app error and database refactor

---
+++ src/internal/db/database.ts
@@ -0,0 +1,16 @@
+import { DataSource } from "typeorm";
+import { CommitInfo } from "./entities/commit_entity";
+import { RepoInfo } from "./entities/repo_entity";
+import { config } from "../config/config";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: config.dbHost,
+  port: config.dbPort,
+  username: config.dbUser,
+  password: config.dbPassword,
+  database: config.dbName,
+  entities: [CommitInfo, RepoInfo],
+  logging: true,
+  synchronize: true,
+});
