78c9c601b7366600a62cb7298a87a50e50b61926
michael anikammadu <dil.anikamadu@gmail.com>
2025-01-11 23:22:37 +0100

    function to set up datastore

--- src/internal/repository/pg_database.ts
+++ src/internal/repository/pg_database.ts
@@ -1,16 +1,22 @@
+import { config, Config } from "./../config/config";
 import { DataSource } from "typeorm";
 import { CommitInfo } from "../db/entities/commit_entity";
 import { RepoInfo } from "../db/entities/repo_entity";
-import { config } from "../config/config";
 
-export const AppDataSource = new DataSource({
-  type: "postgres",
-  host: config.dbHost,
-  port: config.dbPort,
-  username: config.dbUser,
-  password: config.dbPassword,
-  database: config.dbName,
-  entities: [CommitInfo, RepoInfo],
-  logging: true,
-  synchronize: true,
-});
+export function setupDataSource(config: Config): DataSource {
+  const dataSource = new DataSource({
+    type: "postgres",
+    host: config.dbHost,
+    port: config.dbPort,
+    username: config.dbUser,
+    password: config.dbPassword,
+    database: config.dbName,
+    entities: [CommitInfo, RepoInfo],
+    logging: true,
+    synchronize: true,
+  });
+
+  return dataSource;
+}
+
+export const AppDataSource = setupDataSource(config);
