83413e70dc361a2be8f2f9c1184ab6b919377f73
michael anikammadu <dil.anikamadu@gmail.com>
2025-01-14 01:00:48 +0100

    app.ts

---
+++ src/internal/server/app.ts
@@ -0,0 +1,27 @@
+import express, { Express } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { getSettingsRouter } from "./router/settings_router";
+import { getCommitRouter } from "./router/commits_router";
+
+export function initExpressApp(): Express {
+  const app: Express = express();
+
+  app.use(express.json());
+  app.use(
+    express.urlencoded({
+      extended: true,
+    }),
+  );
+
+  app.use(bodyParser.urlencoded({ extended: false }));
+
+  // parse application/json
+  app.use(bodyParser.json());
+
+  app.use(cors());
+  app.use("/settings", getSettingsRouter());
+  app.use("/commits", getCommitRouter());
+
+  return app;
+}
