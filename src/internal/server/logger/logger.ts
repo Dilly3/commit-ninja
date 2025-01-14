a8b3665a704939f23d6c010c77ffdd1a6935153b
michael anikammadu <dil.anikamadu@gmail.com>
2025-01-14 04:38:09 +0100

    logger middleware

---
+++ src/internal/server/logger/logger.ts
@@ -0,0 +1,33 @@
+import chalk from "chalk";
+import { NextFunction, Request, Response } from "express";
+
+chalk.level = 2;
+
+function colorizeMethod(method: string): string {
+  switch (method.toUpperCase()) {
+    case "GET":
+      return chalk.green(method);
+    case "POST":
+      return chalk.yellow(method);
+    case "PUT":
+      return chalk.blue(method);
+    case "DELETE":
+      return chalk.red(method);
+    default:
+      return chalk.white(method);
+  }
+}
+export function loggerMiddleware(
+  req: Request,
+  _: Response,
+  next: NextFunction,
+) {
+  const method = colorizeMethod(req.method);
+  const time = new Date(Date.now()).toString();
+  console.log(
+    `Method:${method}, Host:${chalk.yellow(req.hostname)}, 
+    Path: ${chalk.blue(req.path)}, 
+    Time: ${chalk.bgCyan(time)}`,
+  );
+  next();
+}
